c82c70c9f9784bfa000d1895a894bcfb986fbc48
Chromosomologist <chromosomologist@gmail.com>
2023-06-02 08:56:23 +0200

    feat: implement static db repopulation script

---
+++ scripts/repopulate.py
@@ -0,0 +1,25 @@
+"""Script to repopulate all static databases."""
+
+import asyncio
+
+import database
+
+
+async def _main() -> None:
+    print("Repopulating items...")
+    await database.populate_items(clean=True)
+
+    print("Repopulating tags...")
+    await database.populate_tags(clean=True)
+
+    print("Repopulating characters...")
+    await database.populate_characters(clean=True)
+
+
+def _sync_main() -> None:
+    # NOTE: This is the actual Poetry entrypoint.
+    asyncio.run(_main())
+
+
+if __name__ == "__main__":
+    _sync_main()
